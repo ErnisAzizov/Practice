8dc6f5e9aaa83073397506286b7d392f764e8948
ErnisAzizov <e2r0n0i3s@gmail.com>
2024-05-30 15:28:55 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ from gtts import gTTS
 
 bot = telebot.TeleBot('7364950157:AAGROPIv4C8qvVbmZC1C02iNcE7wtZW3lR8')
 
-#Основные команды бота
-
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id,'Здравствуйте! Добро пожаловать!')
 
-@bot.message_handler(commands=['fullinfo'])
-def main(message):
-    bot.send_message(message.chat.id, message)
+# Перевод текста в речь
 
-#Перевод текста в речь
+user_states = {}
 
 @bot.message_handler(commands=['speech'])
 def main(message):
     bot.send_message(message.chat.id, 'Введите текст')
-    bot.register_next_step_handler(message, text)
+    user_states[message.chat.id] = 'Ожидание'
+
+@bot.message_handler(func=lambda message: user_states.get(message.chat.id) == 'Ожидание')
 def text(message):
-    tts=gTTS(message.text, lang='ru')
-    tts.save(f'{message.from_user.id}.mp3')
-    bot.send_voice(message.chat.id, open(f"{message.from_user.id}.mp3", 'rb'))
+    if message.text.lower() == '/stop':
+        bot.send_message(message.chat.id, 'Процесс приостановлен')
+        user_states[message.chat.id] = None
+    else:
+        tts=gTTS(message.text, lang='ru')
+        tts.save(f'{message.from_user.id}.mp3')
+        bot.send_voice(message.chat.id, open(f"{message.from_user.id}.mp3", 'rb'))
 
 bot.polling(non_stop=True)
